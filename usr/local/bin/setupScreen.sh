c56d57e03f14c5f7ca608b5b5438ac2187a894c9
Timon-Schwarz <timon.anmeldung@gmail.com>
2022-05-11 17:53:00 +0200

    fixed typo

--- usr/local/bin/setupScreen.sh
+++ usr/local/bin/setupScreen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr \\
---output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --rate 59.93 \\
+xrandr \
+--output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --rate 59.93 \
 --output HDMI-1-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --rate "143.98"
