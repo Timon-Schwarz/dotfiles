eb81feba3ed4c34fa058c89df945a2e935b019f9
Timon-Schwarz <timon.anmeldung@gmail.com>
2022-07-08 12:24:36 +0200

    moved neovim to lua

--- home/timon/.config/nvim/lua/config/options.lua
+++ home/timon/.config/nvim/lua/config/options.lua
@@ -1,40 +1,40 @@
 -------------
 -- options --
 -------------
-vim.opt.spell.spelllang = { "en_us", "de_at" }  -- specify spell checking languages
-vim.opt.backup = false                          -- creates a backup file
-vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
-vim.opt.cmdheight = 1                           -- space in the neovim command line for displaying messages
-vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
-vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
-vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
-vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
-vim.opt.ignorecase = true                       -- ignore case in search patterns
-vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
-vim.opt.showmode = false                        -- we don't need to see things like -- INSERT -- anymore
-vim.opt.showtabline = 4                         -- always show tabs
-vim.opt.smartcase = true                        -- smart case
-vim.opt.smartindent = true                      -- make indenting smarter again
-vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
-vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
-vim.opt.swapfile = false                        -- creates a swapfile
-vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
-vim.opt.undofile = true                         -- enable persistent undo
-vim.opt.updatetime = 300                        -- faster completion (4000ms default)
-vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-vim.opt.expandtab = true                        -- convert tabs to spaces
-vim.opt.shiftwidth = 4                          -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 4                             -- insert 2 spaces for a tab
-vim.opt.autoindent = true                       -- automatically indent new lines
-vim.opt.cursorline = true                       -- highlight the current line
-vim.opt.number = true                           -- set numbered lines
-vim.opt.relativenumber = false                  -- set relative numbered lines
-vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-vim.opt.wrap = false                            -- display lines as one long line
-vim.opt.scrolloff = 8                           -- starts scrolling 8 lines before the last visible line
-vim.opt.sidescrolloff = 8                       -- starts scrollling 8 characters before the last character in line
-vim.opt.guifont = "Jetbrains Mono Nerd Font:h11"-- the font used in graphical neovim applications
-vim.opt.wildmode = { "longest", "list", "full" }-- makes command auto completion work more intuitivly
+vim.opt.spell.spelllang = { "en_us", "de_at" }          -- specify spell checking languages
+vim.opt.backup = false                                  -- creates a backup file
+vim.opt.clipboard = "unnamedplus"                       -- allows neovim to access the system clipboard
+vim.opt.cmdheight = 1                                   -- space in the neovim command line for displaying messages
+vim.opt.completeopt = { "menu", "menuone", "noselect" } -- mostly just for cmp
+vim.opt.conceallevel = 0                                -- so that `` is visible in markdown files
+vim.opt.fileencoding = "utf-8"                          -- the encoding written to a file
+vim.opt.hlsearch = true                                 -- highlight all matches on previous search pattern
+vim.opt.ignorecase = true                               -- ignore case in search patterns
+vim.opt.mouse = "a"                                     -- allow the mouse to be used in neovim
+vim.opt.showmode = false                                -- we don't need to see things like -- INSERT -- anymore
+vim.opt.showtabline = 4                                 -- always show tabs
+vim.opt.smartcase = true                                -- smart case
+vim.opt.smartindent = true                              -- make indenting smarter again
+vim.opt.splitbelow = true                               -- force all horizontal splits to go below current window
+vim.opt.splitright = true                               -- force all vertical splits to go to the right of current window
+vim.opt.swapfile = false                                -- creates a swapfile
+vim.opt.termguicolors = true                            -- set term gui colors (most terminals support this)
+vim.opt.undofile = true                                 -- enable persistent undo
+vim.opt.updatetime = 300                                -- faster completion (4000ms default)
+vim.opt.writebackup = false                             -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.expandtab = true                                -- convert tabs to spaces
+vim.opt.shiftwidth = 4                                  -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4                                     -- insert 2 spaces for a tab
+vim.opt.autoindent = true                               -- automatically indent new lines
+vim.opt.cursorline = true                               -- highlight the current line
+vim.opt.number = true                                   -- set numbered lines
+vim.opt.relativenumber = false                          -- set relative numbered lines
+vim.opt.signcolumn = "yes"                              -- always show the sign column, otherwise it would shift the text each time
+vim.opt.wrap = false                                    -- display lines as one long line
+vim.opt.scrolloff = 8                                   -- starts scrolling 8 lines before the last visible line
+vim.opt.sidescrolloff = 8                               -- starts scrollling 8 characters before the last character in line
+vim.opt.guifont = "Jetbrains Mono Nerd Font:h11"        -- the font used in graphical neovim applications
+vim.opt.wildmode = { "longest", "list", "full" }        -- makes command auto completion work more intuitivly
 
 
 
--- home/timon/.config/nvim/lua/config/plugins.lua
+++ home/timon/.config/nvim/lua/config/plugins.lua
@@ -41,15 +41,36 @@ packer.init {
 -- plugins --
 -------------
 return packer.startup(function(use)
-    -- install your plugins here
+    -- plugin management
     use "wbthomason/packer.nvim"          -- have packer manage itself
+
+    -- plugin dependencies
     use "nvim-lua/popup.nvim"             -- some plugins require this: an implementation of the Popup API from vim in Neovim
     use "nvim-lua/plenary.nvim"           -- some plugins require this: generally useful lua functions
+
+    -- cholorschemes
     use "dracula/vim"                     -- allows to use the dracula color shema
+
+    -- UI addons
     use "vim-airline/vim-airline"         -- adds some UI elements
     use "vim-airline/vim-airline-themes"  -- adds theming for the vim-airline plugin
     use "kyazdani42/nvim-web-devicons"    -- adds icons
+    
+    -- file explorer
     use "kyazdani42/nvim-tree.lua"        -- file explorer
+
+    -- completion
+    use "hrsh7th/nvim-cmp"                -- The completion plugin
+    use "hrsh7th/cmp-buffer"              -- buffer completions
+    use "hrsh7th/cmp-path"                -- path completions
+    use "hrsh7th/cmp-cmdline"             -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip"        -- snippet completions
+
+    -- snippets
+    use "L3MON4D3/LuaSnip"                -- snippet engine
+    use "rafamadriz/friendly-snippets"    -- a bunch of snippets to use
+
+    -- LaTeX
     use {
         "lervag/vimtex",                  -- plugin for LaTeX writing
         ft = { "tex" }                    -- load plugin only when opening .tex file
